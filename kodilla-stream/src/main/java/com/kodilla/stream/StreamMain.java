5d8f41080b69cf5bdc36db7b0642968e57f28232
paluchowskitomasz <paluchowski.tomasz@gmail.com>
2020-02-26 21:17:33 +0100

    Java Plus 2019-10 (dashboard) 8. Obsługa błędów w Javie (Moduł) 8.2. try! catch! finally!

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,14 @@ package com.kodilla.stream;
 import com.kodilla.stream.array.ArrayOperations;
 import com.kodilla.stream.book.Book;
 import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.portfolio.Board;
+import com.kodilla.stream.portfolio.Task;
+import com.kodilla.stream.portfolio.TaskList;
+import com.kodilla.stream.portfolio.User;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 
